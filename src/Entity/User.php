5e5d42133bc93a5fa0330c942ed06f7243b0fbef
mids <mehdibenabbou95@gmail.com>
2022-03-22 16:07:09 +0100

    modif user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints  as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- *  @ORM\table(name="`user`")
+ *  @ORM\Table(name="`user`")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
